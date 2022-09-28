e933201635f1be13f75736bcb165fb2cd40246f6
MVaisal <22.m.faisal@gmail.com>
2022-09-28 20:24:52 +0700

    Update websites.js

--- websites.js
+++ websites.js
@@ -8,27 +8,7 @@
 **/
 module.exports = [
     {
-        url: 'http://node-ping.herokuapp.com',
-        interval: 15
-    },
-    {
-        url: 'http://video-download.herokuapp.com',
-        interval: 15
-    },
-    {
-        url: 'https://nodeza.co.za',
-        interval: 15
-    },
-    {
-        url: 'https://blog.ragingflame.co.za',
-        interval: 12
-    },
-    {
-        url: 'https://ragingflamesolutions.co.za',
-        interval: 15
-    },
-    {
-        url: 'https://nodetube.ragingflame.co.za',
-        interval: 15
+        url: 'https://rawon.bucoks1.repl.co',
+        interval: 1
     }
 ];
